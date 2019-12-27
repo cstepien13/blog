282f3ddc4af2f8c178577cd692459713c3219b71
cstepien13 <59266572+cstepien13@users.noreply.github.com>
2019-12-26 20:15:12 -0800

    Colour Change

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,7 +61,7 @@ module.exports = {
         name: `Gatsby Starter Blog`,
         short_name: `GatsbyJS`,
         start_url: `/`,
-        background_color: `#ffffff`,
+        background_color: `#bdccbc`,
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `content/assets/gatsby-icon.png`,
